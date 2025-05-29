285037689ab36fc6d2d081edb868caaadca00432
Juanjo200599 <juansalazar292005@gmail.com>
2025-05-29 17:28:24 -0500

    todas la scapas de usuario listas y tambien todos los metoditos probados en postman

--- src/models/usuarios.ts
+++ src/models/usuarios.ts
@@ -4,9 +4,11 @@ import { ObjectId } from "mongodb";
 export class Usuario{
     _id: ObjectId;
     nombre:string;  
+    edad:number;
 
-constructor(id:ObjectId,name:string){
-    this._id = id;
-    this.nombre=name;
+constructor(id:ObjectId,name:string,edad:number){
+   this._id = id;
+     this.nombre=name;
+    this.edad=edad
 }
 }
