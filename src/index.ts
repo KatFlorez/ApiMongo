f0a2ea2f66fa5f4a29337eef0afd5ccd708bddcf
anny <anny.cardeno@correo.tdea.edu.co>
2025-05-31 09:49:20 -0500

    subir

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
 app.use('/api/pedido', pedidorouter);
-app.use('/api/usuario',usuariorouter );
+app.use('/api/usuarios',usuariorouter );
 
 
 
